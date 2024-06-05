f0382b790dab38eaea06127fe14fda7063dd22ef
mchittimalla <40994387+mchittimalla@users.noreply.github.com>
2024-06-05 18:54:29 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Senior Cloud DevOps Engineer", "Certified Azure Admin Developer", "Ansible Solution Architect", "Backend development", "Terraform Developer"],
+    strings: ["Senior Cloud DevOps Engineer", "Certified Azure Admin Developer", "Ansible Solution Architect", "Backend Developer", "Terraform Developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
